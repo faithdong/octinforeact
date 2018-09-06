b960db516703a7233b020e1ae2ea5e017e028518
zhongxiaodong <zxdzyadmin@yonyou.com>
2018-09-06 17:47:41 +0800

    add

---
+++ src/pages/equip/Equip.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: zhongxd 
+ * @Date: 2018-09-06 17:22:23 
+ * @Last Modified by: zhongxd
+ * @Last Modified time: 2018-09-06 17:38:03
+ */
+
+import React from 'react';
+
+class Equip extends React.Components{
+
+}
+
+export default Equip;
